52b2152736ba6a13a65ba4f2738875216ab279e0
Joseph Parker <jparker010190@gmail.com>
2018-05-31 01:24:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(:username => params[:username])
+    @user = User.find_by(username: params[:username])
     binding.pry
     if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
