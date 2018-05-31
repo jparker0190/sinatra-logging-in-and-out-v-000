320e0bdc9eada0a6c73ba070f55da7008393e492
Joseph Parker <jparker010190@gmail.com>
2018-05-31 00:47:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
 
 
   get '/account' do
-    @user = User.current_user(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
     if @user
       erb :account
     end
